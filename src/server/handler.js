4953ccd2d7c8fb93e377ff625cfa1010d64e5203
DimasAnugerahh <dimasanugrah883@gmail.com>
2024-12-22 13:21:40 +0800

    initial commit

---
+++ src/server/handler.js
@@ -0,0 +1,59 @@
+const predictClassification = require('../services/inferenceService');
+const crypto = require('crypto');
+const storeData = require('../services/storeData');  
+
+async function postPredictHandler(request, h) {
+  const { image } = request.payload; 
+  const { model } = request.server.app;  
+
+  let confidenceScore, label, suggestion;
+
+  try {
+    const result = await predictClassification(model, image);
+    confidenceScore = result.confidenceScore;
+    label = result.label;
+    suggestion = result.suggestion;
+  } catch (error) {
+    return h.response({
+      status: 'fail',
+      message: 'Terjadi kesalahan dalam melakukan prediksi',
+    }).code(400);  
+  }
+
+  const id = crypto.randomUUID();
+  const createdAt = new Date().toISOString();
+
+  const data = {
+    id,
+    result: label,  
+    suggestion,
+    createdAt
+  };
+
+  try {
+    await storeData(id, data); 
+  } catch (error) {
+    return h.response({
+      status: 'fail',
+      message: 'Terjadi kesalahan saat menyimpan hasil prediksi ke database.',
+    }).code(500);  
+  }
+
+  const message = "Model is predicted successfully";
+
+  const response = h.response({
+    status: 'success',
+    message: message,
+    data: {
+      id,
+      result: label,
+      suggestion: suggestion,
+      createdAt
+    }
+  });
+
+  response.code(201);  
+  return response;
+}
+
+module.exports = postPredictHandler;
